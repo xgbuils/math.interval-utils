dcab4327c24e17feab8bb8ddc3f8330db5766c87
Xavier Garcia Buils <xgbuils@gmail.com>
2017-01-10 23:34:44 +0100

    expose new functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+const areDisjoint = require('./are-disjoint')
+const areEqual = require('./are-equal')
+const contains = require('./contains')
+const intersection = require('./intersection')
 const isEmpty = require('./is-empty')
 const isInterval = require('./is-interval')
-const contains = require('./contains')
+const multiIntersection = require('./multi-intersection')
 const numToInterval = require('./num-to-interval')
 const parser = require('./parser')
+const relativeComplement = require('./relativeComplement')
 const union = require('./union')
 
 module.exports = {
+	areDisjoint: areDisjoint,
+	areEqual: areEqual,
+	contains: contains,
+	intersection: intersection,
     isEmpty: isEmpty,
     isInterval: isInterval,
-    contains: contains,
+    multiIntersection: multiIntersection
     numToInterval: numToInterval,
     parser: parser,
+    relativeComplement: relativeComplement
     union: union
 }
